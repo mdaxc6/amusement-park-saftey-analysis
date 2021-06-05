15b0bcc1f8179c3a462e9a78add746cf8d3a3ccb
Jordan Wright <jordanwright@Jordans-MBP.attlocal.net>
2021-06-05 15:39:41 -0500

    plot.js file added

---
+++ plot.js
@@ -0,0 +1,19 @@
+const url = "https://park-accidents-api.herokuapp.com/api/v1.0/all_accidents";
+
+// Fetch the JSON data and console log it
+d3.json(url).then(function(data) {
+  console.log(data);
+});
+
+// Promise Pending
+const dataPromise = d3.json(url);
+console.log("Data Promise: ", dataPromise);
+
+
+/*#####Building plots###########*/
+
+
+function buildPlot(){
+  d3.(url).then(function(data){}
+    
+  }
