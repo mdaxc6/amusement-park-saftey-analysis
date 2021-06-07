8a7b9dfb8887e642b5aee12f0d24cdbb8ef4f8db
Jordan Wright <jordanwright@Jordans-MacBook-Pro.local>
2021-06-07 18:27:36 -0500

    edits to plot.js and plot.html

--- plot.js
+++ plot.js
@@ -15,10 +15,49 @@ console.log("Data Promise: ", dataPromise);
 
 /*Accidents/time, Device type/accidents, Business type/accident, name/accidents */
 
-function buildplot(){
-  d3.json(url).then(function(response) {
-    var data = response.dataset.data;
+// Function to display the plot
+function plotData() {
+  // Select the node using d3
+  var selectNode = d3.select("selDataset");
 
-  }
-  
+  var accident = selectNode.property("acc_date");
+
+
+  var device_type = data[device_type];
+
+  // Create arrays 
+  var xData = Object.values(data.device_type);
+  var yData = Object.values(data.acc_date);
+
+  // Create a trace using the platform keys and values
+  var trace = {
+      x: xData,
+      y: yData,
+      type: "bar"
+  };
+
+  var chartData = [trace];
+
+  // Return data to form chart
+  return chartData;
 }
+
+function updatePlot() {
+  var chartData = plotData();
+
+  // Use `Plotly.react()` to update plot
+  Plotly.react("chartDiv", chartData);
+}
+
+// Function to create initial chart
+function init() {
+  var chartData = plotData();
+  var layout = {height: 600, width:800};
+  Plotly.newPlot("chartDiv", chartData, layout);
+}
+
+// On change to the DOM, call updatePlot()
+d3.selectAll("selDataset").on("change", updatePlot);
+
+// Call `init()` function
+init();
